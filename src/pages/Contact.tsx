78ced02812f5c531ca33a3f8e4667ed4fb780920
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2024-03-21 12:40:55 -0300

    Fix spin

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -185,7 +185,7 @@ const Contact: React.FC = () => {
                     className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-green-300 border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
                     role="status"
                   >
-                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    <span className="absolute -m-px !h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">
                       Loading...
                     </span>
                   </div>
