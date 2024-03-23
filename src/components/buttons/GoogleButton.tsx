77d6d2cc554bbef117cd18de4e50880731e65ebb
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2024-03-23 18:03:54 -0300

    different login method test

--- src/components/buttons/GoogleButton.tsx
+++ src/components/buttons/GoogleButton.tsx
@@ -1,4 +1,4 @@
-import googleLogo from "../../assets/google.png";
+import { useState } from "react";
 import {
   GoogleAuthProvider,
   onAuthStateChanged,
@@ -9,12 +9,15 @@ import { appFireBase } from "../../firebase/firebase.js";
 import { useNavigate } from "react-router";
 import { useData } from "../../context/userContext.js";
 import { useEffect } from "react";
+import Spinner from "../Spinner.js";
+import googleLogo from "../../assets/google.png";
 
 const auth = getAuth(appFireBase);
 
 const GoogleButton: React.FC = () => {
   const { setUserSession } = useData();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -29,7 +32,10 @@ const GoogleButton: React.FC = () => {
           pictureUrl: user.photoURL || "",
         };
         setUserSession(googleUser);
+
         navigate("/");
+      } else {
+        setLoading(false);
       }
     });
 
@@ -40,6 +46,7 @@ const GoogleButton: React.FC = () => {
     const provider = new GoogleAuthProvider();
 
     try {
+      setLoading(true);
       signInWithRedirect(auth, provider);
     } catch (error) {
       console.error(error);
@@ -48,16 +55,20 @@ const GoogleButton: React.FC = () => {
 
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm flex w-full items-center justify-center">
-      <button
-        type="submit"
-        onClick={googleSignIn}
-        className="h-12 flex w-full items-center justify-center bg-white rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border border-gray-300 hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300 transition-colors duration-200 ease-in-out"
-      >
-        <>
-          <img src={googleLogo} alt="Google Logo" className="w-6 h-6 mr-4" />
-          Sign in with Google
-        </>
-      </button>
+      {!loading ? (
+        <button
+          type="submit"
+          onClick={googleSignIn}
+          className="h-12 flex w-full items-center justify-center bg-white rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border border-gray-300 hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300 transition-colors duration-200 ease-in-out"
+        >
+          <>
+            <img src={googleLogo} alt="Google Logo" className="w-6 h-6 mr-4" />
+            Sign in with Google
+          </>
+        </button>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 };
