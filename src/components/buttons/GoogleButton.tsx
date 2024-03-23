ebfcdc6ab777464537996dedb02a338ed0dd5be1
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2024-03-23 12:18:39 -0300

    fix login

--- src/components/buttons/GoogleButton.tsx
+++ src/components/buttons/GoogleButton.tsx
@@ -3,25 +3,24 @@ import {
   GoogleAuthProvider,
   UserCredential,
   getAuth,
-  getRedirectResult,
-  signInWithRedirect,
+  signInWithPopup,
 } from "firebase/auth";
 import { appFireBase } from "../../firebase/firebase.js";
 import { useNavigate } from "react-router";
 import { useData } from "../../context/userContext.js";
-import { useEffect, useState } from "react";
 
 const auth = getAuth(appFireBase);
 
 const GoogleButton: React.FC = () => {
   const { setUserSession } = useData();
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getRedirectResult(auth)
-      .then((result: UserCredential | null) => {
-        if (result) {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    try {
+      signInWithPopup(auth, provider)
+        .then((result: UserCredential) => {
           const user = result.user;
           const displayNameParts = user.displayName
             ? user.displayName.split(" ")
@@ -33,22 +32,11 @@ const GoogleButton: React.FC = () => {
             pictureUrl: user.photoURL || "",
           };
           setUserSession(googleUser);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-        navigate("/");
-      });
-  }, []);
-
-  const googleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-
-    try {
-      signInWithRedirect(auth, provider);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } catch (error) {
       console.log(error);
     }
@@ -56,18 +44,16 @@ const GoogleButton: React.FC = () => {
 
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm flex w-full items-center justify-center">
-
-        <button
-          type="submit"
-          onClick={googleSignIn}
-          className="h-12 flex w-full items-center justify-center bg-white rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border border-gray-300 hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300 transition-colors duration-200 ease-in-out"
-        >
-          <>
-            <img src={googleLogo} alt="Google Logo" className="w-6 h-6 mr-4" />
-            Sign in with Google
-          </>
-        </button>
-   
+      <button
+        type="submit"
+        onClick={googleSignIn}
+        className="h-12 flex w-full items-center justify-center bg-white rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border border-gray-300 hover:bg-green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300 transition-colors duration-200 ease-in-out"
+      >
+        <>
+          <img src={googleLogo} alt="Google Logo" className="w-6 h-6 mr-4" />
+          Sign in with Google
+        </>
+      </button>
     </div>
   );
 };
